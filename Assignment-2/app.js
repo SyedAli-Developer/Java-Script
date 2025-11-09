9378902e6d74449d87f07358217adc36c742e147
SyedAli-Developer <syedali.h.b.g.626@gmail.com>
2025-11-09 13:36:32 +0500

    make a operator solver machine

---
+++ Assignment-2/app.js
@@ -0,0 +1,6 @@
+var fequation = +prompt("Enter your first equation.")
+var operator = +prompt("Enter your operator")
+var sequation = +prompt("Enter your second equation.")
+var result = (fequation + operator + sequation)
+alert("This is your equation solution: " + fequation + operator + sequation + "is equal to: " + result)
+
