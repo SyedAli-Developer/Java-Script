74a0ee1c7a4a002760f529e582d29d997dbfd445
SyedAli-Developer <syedali.h.b.g.626@gmail.com>
2026-04-18 15:42:59 +0500

    practice exercise 2

--- Practice loops and conditions/app.js
+++ Practice loops and conditions/app.js
@@ -1,35 +1,5 @@
-// for (let i = 1; i<= 100; i++){
-//     if (i % 2 === 0) {
-//         console.log(`${i} is Even`);
-
-//     }else{
-
-//         console.log(i + ' is Odd');
-//     }
-
-// }
-
-// chellange two 
-
-// for( let i = 1; i <= 10 ; i++)
-// {
-//         console.log(`5 x ${i} = ${5 * i}`)
-// }
-
-
-// challenge 3
-
-// for (let i = 1; i <= 15; i++) {
-//     if (i % 3 == 0 && i % 5 === 0) {
-//         console.log(i + " FizzBuzz")
-//         continue
-//     }
-//     if (i % 5 === 0) {
-//         console.log(i + " Buzz")
-//     }
-//     if (i % 3 === 0) {
-//         console.log(i + " Fizz")
-//         continue
-//     }
-//     console.log(i)
-// }
+for (let i = 10; i>0; i--)
+{
+    console.log(i)
+}
+console.log("Happy Coding")
