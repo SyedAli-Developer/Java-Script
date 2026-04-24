93b758f3ab34336c3b8f48215ea15ceafd695c90
SyedAli-Developer <syedali.h.b.g.626@gmail.com>
2026-04-24 18:50:38 +0500

    changes

--- New-Practice/JS-Practice/app.js
+++ New-Practice/JS-Practice/app.js
@@ -1,2 +1,6 @@
-let btn = document.querySelector('button')
-btn.removeAttribute('disabled')
+let box = document.querySelector('body')
+let div = document.querySelector('#div')
+let h1 = document.createElement('h1')
+h1.textContent = 'Hello World of motherfuckingtash'
+console.log(h1)
+div.append(h1)
